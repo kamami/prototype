b2dd1e4f26016d59962da8b763039aeb67555a95
kamami <seubert.mar@gmail.com>
2019-01-13 23:38:56 +0100

    hjfjhgjh

--- src/components/ViewAll.js
+++ src/components/ViewAll.js
@@ -132,7 +132,9 @@ render() {
                 <div>
 
 
-
+                  <Media query="(min-width: 1025px)">
+                        {matches =>
+                          matches ? (
 
 
                       <Card className="ItemViewAll" style={{backgroundImage: 'url(' + title.image + ')', boxShadow: 'none'}}>
@@ -147,6 +149,22 @@ render() {
 
 
                 </Card>
+              ):(
+                <Card className="ItemViewAllMobile" style={{backgroundImage: 'url(' + title.image + ')', boxShadow: 'none'}}>
+                  {title.select &&
+                  <div style={{display: 'flex', width: '100%',backgroundColor: 'rgba(255, 255, 255, 0.8)', height: 36, marginTop: -1 }}>
+                    <p style={{marginLeft: 'auto', marginRight: 'auto', marginTop: '1%', marginBottom: '1%', fontFamily: 'Pacifico', color: '#00C9B7', fontSize: '1.4em', lineHeight: '1.6'}}> Fable.Select</p>
+                  </div>
+                  }
+
+              <div className="overlay" onClick = {()=> {history.push('/bots/' + title.id)}}>
+                </div>
+
+
+            </Card>
+
+              )}
+            </Media>
 
 
 
